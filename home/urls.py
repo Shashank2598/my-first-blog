43227197677a70cd179f42a89c5a387719bf1f62
shashank2598 <shashank2598@gmail.com>
2017-11-11 17:44:13 +0530

    ninth

--- home/urls.py
+++ home/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r'^addposts/$',views.postCreate.as_view(),name = 'addposts'),
     url(r'^home/(?P<pk>[0-9]+)/$',views.detailview.as_view(),name = 'detailview'),
     url(r'^logout/$',views.logoutkrdo,name='logoutkrdo'),
-    url(r'^profile/(?P<id>[0-9]+)/$',views.profileupdate.as_view(),name='profile')
+  
 ]
